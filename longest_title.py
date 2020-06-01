6ccd133839f70c00b0f6dade1e7f53323482823d
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-01 00:25:31 -0400

    optional run editor

--- longest_title.py
+++ longest_title.py
@@ -8,6 +8,8 @@ Michael Hirsch, Ph.D.
 """
 
 import argparse
+import subprocess
+import shutil
 from pathlib import Path
 
 import hugoutils
@@ -18,16 +20,24 @@ def main():
     p.add_argument("path", help="path to sort longest title")
     p.add_argument("-n", help="show titles of more than N characters", type=int, default=60)
     p.add_argument("-ext", help="filename suffix", default=".md")
+    p.add_argument("-c", help="run program for this file", choices=["gedit", "notepad++", "code"])
     p = p.parse_args()
 
     inpath = Path(p.path).expanduser()
     if not inpath.is_dir():
         raise NotADirectoryError(inpath)
 
+    if p.c:
+        exe = shutil.which(p.c)
+        if not exe:
+            raise FileNotFoundError(f"{p.c} not found")
+
     for f in inpath.glob(f"**/*{p.ext}"):
         title = hugoutils.get_header(f)[0]["title"]
         if len(title) > p.n:
             print((f.name, title))
+            if exe:
+                subprocess.run([exe, str(f)])
 
 
 if __name__ == "__main__":
